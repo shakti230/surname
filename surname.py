4a4f0946731543d43bf14d0a374500057022a0a0
shakti230 <36244887+shakti230@users.noreply.github.com>
2018-04-07 19:37:26 +0100

    Update surname.py

--- surname.py
+++ surname.py
@@ -1,6 +1,21 @@
 # Mary McDonagh
 # A program that displays Fibonacci numbers using people's names.
+#Exercise 1 - A program that displays Fibonacci numbers.
+#def fib(n):
+#  """This function returns the nth Fibonacci number."""
+#  i = 0
+#  j = 1
+#  n = n - 1
+#  while n >= 0:
+#    i, j = j, i + j
+#    n = n - 1 
+#  return i
+# Test the function with the following value.
+#x = 38
+#ans = fib(x)
+#print("Fibonacci number", x, "is", ans)
 
+#Exercise 2 - Fibonacci Surname Problem
 def fib(n):
   """This function returns the nth Fibonacci number."""
   i = 0
